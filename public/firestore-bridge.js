8792cd6b8a8af64694fd925d82094200e964aada
yasserA5 <134278379+yasserA5@users.noreply.github.com>
2026-04-27 21:03:45 +0100

    Add files via upload

--- public/firestore-bridge.js
+++ public/firestore-bridge.js
@@ -1,13 +1,18 @@
-import { db } from "./firebase-init.js";
-import { auth } from "./firebase-init.js";
+import { db, auth } from "./firebase-init.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  deleteDoc,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 onAuthStateChanged(auth, (user) => {
   console.log("SAVE PAGE auth user:", user ? user.uid : null);
 });
-
-import { collection, addDoc, getDocs, query, orderBy, deleteDoc, doc }
-  from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 window.fs = {
   db,
