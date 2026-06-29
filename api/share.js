d4920c772fcc27a56febcb766ba1ac0a83bbfd2b
yasserA5 <134278379+yasserA5@users.noreply.github.com>
2026-06-29 17:52:38 +0100

    Add files via upload

--- api/share.js
+++ api/share.js
@@ -3,9 +3,9 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
- "project_id": "ofouq-academie",
-  "client_email": "firebase-adminsdk-fbsvc@ofouq-academie.iam.gserviceaccount.com",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDJk/qH386v6N9g\noRMSMmuqIh+WhokZDXOVn0aNE3J8jqNDkghUU4Op+Oe3KVGNum2Ss25WifO0V6cZ\nLKP99PvJzeU1Z8aRTY1jHg2CJXDKsljSndH0YfWWkfT8JMNpUJSGhVq60XxLCnkG\nz2SvJ8rgBudN03kYr1MqvKoLQnq6C+HLFBred8Sh1keMdDmX1Z6qd1US7rlUwWEo\ni2P2dru34NA+b1knhDOH8scOEPt1LBTKVYoxFZ5s7ZvWW39+rybWKigvQaJaViKa\nTjPWZyf3ZjOvkYxSMH6oZE/IPzAHGXU0MlRPPtj2Je0q1V+WTEEHH+/6jPDYPVLb\ntq51r/s3AgMBAAECggEAH0iq+gle72gJutpktk9C7E2Nb3QDtPKkfJUSgvSAsRoF\n9oekaUnoxilN9Nwhvwgu1zSnrCCOxO9RjJOkA9bQeNpOd0Jay9QoXDieyhL7dUnG\nhTFfEzlcvvh59+PQu/h4hIQPt6ypSJAghlELhC99ydTocuieA5m6I4jSLxsrJtJI\nvmXnlO4Myi8Mf73akhvSkBL45mrUjlRrybxNCASe15s8wewAGdCxonQJPT8loWw+\nJDtM8z35lDPxWPdThojfWIvkG2RC2i0FlEvUAdGBmkmf0jD9K78iaDfd9XrmYezQ\n0OZHvNogn8MTtQgpK8N92InsZ7WyH4B6VFbIYVOryQKBgQDkwW0PCkkVyUEmuZ7Q\nIwH4iwBDoECvUqXLlk/ZHWoZfVPeRX/gNJ+R4LWp9o4UP4otxdZfAHqGb7l/C62s\nXI1hlH3L3obehKg3qQysQ0k7A9lMBFnWm99FvqnSv/afZBqUA0SC8mTvOgmTS0DY\nS7jLvVvmdauQPxyz1eJ8TIcijQKBgQDhle56YL0ZgkeweMSXPaUkzdoPk1tVHDv/\n7xsuHXGkkFOZhb2CnlqDuREs3bZT5xTQBpmRKdZMD30dmXSaE8zm0n5CNRWFOhWF\noHOIsOefyVxGSTZEGm8rluNvVNPoSBpr9VMvV7hTJdsTEYuQILPJJwmbfJ9JQs5F\nP1aupZjF0wKBgQCLzsPOq2SbkhLljhDH+H1d3+ZbbnfaL1LxKH7InTeZOIWnlSZg\nawkTaRjeNbgEHAlhjEZGbc50l5fxU7vjWfV2rABuNySKnt0Il6MNLVp2XRXTZd1a\nUo2U3MCB85zRMackyxJ2TZrusrPjd9y71RuuOZ8PLN+cG1Z5esEfYs48iQKBgFrz\nbzYKANPjEfJVjrWHliizkkKLX9x9yLcUeTnrhyEZS4tvP4wFOms3V3RmY5eKaHrC\n3nGHk64Q5Jnlf/yLFSbPITaiXm3bkEfS/CHVaV2j+r8fz3A/1CiX6lS9t75EK9rx\nCzvgOyHS9nPGtCVE+heU1KvK4BHSjIWg1tsNSv4fAoGAMLEG2meI2AFv2xUFsXLd\nkDZQmhI0l0ZspX+1Z0+plvrGDaD9yI0wqmTlqPvXwfM7oYAmFo7iBNUxYTHfKhG2\nBoHEAgW6qwnlnnXTRT0wM0w60hHYlwz5QOJ0zIUqL0yIkle/s4NmXGOKly+PTQxA\nFWn8vGJtM651bD7w/M2f1uQ=\n-----END PRIVATE KEY-----\n",
+ "projectId": "ofouq-academie",
+  "clientEmail": "firebase-adminsdk-fbsvc@ofouq-academie.iam.gserviceaccount.com",
+  "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDCGLIf/25YRN/Z\nxwL6sfukwdL5EUwDAQOgjZaSziE2cMoTdQ5xo+jaG/czHrJ08IY1CAoETvlpuktE\nbk9avlzEEaV7XM0DTF1OKmOKH5byoHFw0C4fhDgs8aGNQJN1tywPttff+DcISaIy\nTqmUpP2s9Bgh0LlQi5J7Ot5+VmZYVVslpAnsZteRGd4mQTyTo1d4fZkseaSFDVMw\ngfexQKfusxO8LQzNwl7rNecEAS21qlv+Cb2xMKXiYpLEEmC0+ntNMQUteKJfaufp\n0BjbGoSKvyqegRnBCQIF+zbLU23YbEd/nflY4wfC/ptyxpA7JPZ/+IgZ2ibjCt+v\na+4HqRd7AgMBAAECggEAIDs/oSamqUvOrHhq2zyhICv7n9aqe7k4Y/n/LiPCyiqS\ntAZ1PpPMgCOyui93zQmAD+Bk4XBH5LHF+/ghf10rRHf1tfWkKc8Nk4RggThbtbDF\nBcncTumC9putiypVEnA3heiEErJCWUL5B0eey4tWgNkMBNmHXIPA8GvHNM2eMz63\nELepom2SpyCpwZaEVkU7H3magCCMKAvIXaqhoMaS/N+oBAahOly5g7XZkysuFsxd\n4V+cS/N2lzoKBYs6FPx9aWbzwfozAMwDzBtzCLqwQAVfUJYW1nLNRZlUYBYi/jcV\nNLOZZj3KSdkYevQU0vb1QTepqu4Gk9EXtzUku+3+yQKBgQDzeXrtllJg0hf3h2oA\nBeYWODb5OQVyIydpzSgb7Bnl63CNVD2/MqX3Xj7s4ux+AKdMScG0lRcJaphDxwYs\n6ysuzW7PfZqkN0riciSLLg8Hi5+4BgpHd52LiSlhpCa6AwcQvHKZ7lbInCm0X6qA\nINIc+9pGxq/4NtBekWVXHZLK1QKBgQDMFOohrH+P1B+i9m8IIEYUGA7gRN2Yq26k\nCC0cZsvS5EPYS/lvgKwb5AO4Q7xgDxgTKw+Ei3ybQIUlMrKWuIHIC0O5In7Ryuc/\nHok05W+ULGnRuSbJVd8zxGLCCulavnVvUQpwMRmzeyb9a5bzr6D20k9xvp/tGDTe\nrzZpuaLhDwKBgCUuL++IPRw7+Rz7uNw4CmLqQrrcUlCTvbkSc0WQUIQj6BQtASDp\ngx67bbCPFr5HK0UjHkmNWu89L/NpuS+y2e1TwdA9xhe9udnKxHnclr74O3PGuFsh\nkRV15LPcdW2CeNm9bWLEWsxXzKaW1VqrqGp0q2iTftvQwxEt8uPSyE/hAoGARGlK\nZwldJYi1Jjq306CF/VxnODS9l9z31NLM1HV9I+/S8mHddsxKZXsU6CMEtaq5pktM\nA2GC3sRNHcAV78Jf9wAXrqrqmkcLJ7SDyBCVS7Vrr5sCboFztfXCKUvhF8M6uJsR\nBbTf6bOByXfUfVHFgpfTe8yr7Z2ss21Gr0BEYm8CgYEAz+0f6IOZ/IyrgPLVuy9y\n/Lud0cffw8eY6n7+BGaBz8PIbY9x58dfdZ2oQUMd9AyjnqIQwOQYDMWSQgQistqG\nX4NaSMnjWxtHBK5Dmmo8psoQAFm1cUEtZQHa+wmq7eToPX19z6lOSEj7p9GgMyZu\nU8jPw2kR13I4xKuqA5FqluE=\n-----END PRIVATE KEY-----\n",
     })
   });
 }
@@ -21,7 +21,7 @@ const docId = req.query.docId || req.query.id;
       return res.status(400).send("Missing document ID");
     }
 
-    const docRef = db.collection("articles").doc(docId);
+const docRef = db.collection("content").doc(docId);
     const snap = await docRef.get();
 
     if (!snap.exists) {
