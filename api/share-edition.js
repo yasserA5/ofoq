51c646d035aa070c9b9a5dd43ee40dd2acfc95eb
yasserA5 <134278379+yasserA5@users.noreply.github.com>
2026-06-30 01:46:20 +0100

    Add files via upload

--- api/share-edition.js
+++ api/share-edition.js
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: "ofouq-academie",
       clientEmail: "firebase-adminsdk-fbsvc@ofouq-academie.iam.gserviceaccount.com",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDCGLIf/25YRN/Z\nxwL6sfukwdL5EUwDAQOgjZaSziE2cMoTdQ5xo+jaG/czHrJ08IY1CAoETvlpuktE\nbk9avlzEEaV7XM0DTF1OKmOKH5byoHFw0C4fhDgs8aGNQJN1tywPttff+DcISaIy\nTqmUpP2s9Bgh0LlQi5J7Ot5+VmZYVVslpAnsZteRGd4mQTyTo1d4fZkseaSFDVMw\ngfexQKfusxO8LQzNwl7rNecEAS21qlv+Cb2xMKXiYpLEEmC0+ntNMQUteKJfaufp\n0BjbGoSKvyqegRnBCQIF+zbLU23YbEd/nflY4wfC/ptyxpA7JPZ/+IgZ2ibjCt+v\na+4HqRd7AgMBAAECggEAIDs/oSamqUvOrHhq2zyhICv7n9aqe7k4Y/n/LiPCyiqS\ntAZ1PpPMgCOyui93zQmAD+Bk4XBH5LHF+/ghf10rRHf1tfWkKc8Nk4RggThbtbDF\nBcncTumC9putiypVEnA3heiEErJCWUL5B0eey4tWgNkMBNmHXIPA8GvHNM2eMz63\nELepom2SpyCpwZaEVkU7H3magCCMKAvIXaqhoMaS/N+oBAahOly5g7XZkysuFsxd\n4V+cS/N2lzoKBYs6FPx9aWbzwfozAMwDzBtzCLqwQAVfUJYW1nLNRZlUYBYi/jcV\nNLOZZj3KSdkYevQU0vb1QTepqu4Gk9EXtzUku+3+yQKBgQDzeXrtllJg0hf3h2oA\nBeYWODb5OQVyIydpzSgb7Bnl63CNVD2/MqX3Xj7s4ux+AKdMScG0lRcJaphDxwYs\n6ysuzW7PfZqkN0riciSLLg8Hi5+4BgpHd52LiSlhpCa6AwcQvHKZ7lbInCm0X6qA\nINIc+9pGxq/4NtBekWVXHZLK1QKBgQDMFOohrH+P1B+i9m8IIEYUGA7gRN2Yq26k\nCC0cZsvS5EPYS/lvgKwb5AO4Q7xgDxgTKw+Ei3ybQIUlMrKWuIHIC0O5In7Ryuc/\nHok05W+ULGnRuSbJVd8zxGLCCulavnVvUQpwMRmzeyb9a5bzr6D20k9xvp/tGDTe\nrzZpuaLhDwKBgCUuL++IPRw7+Rz7uNw4CmLqQrrcUlCTvbkSc0WQUIQj6BQtASDp\ngx67bbCPFr5HK0UjHkmNWu89L/NpuS+y2e1TwdA9xhe9udnKxHnclr74O3PGuFsh\nkRV15LPcdW2CeNm9bWLEWsxXzKaW1VqrqGp0q2iTftvQwxEt8uPSyE/hAoGARGlK\nZwldJYi1Jjq306CF/VxnODS9l9z31NLM1HV9I+/S8mHddsxKZXsU6CMEtaq5pktM\nA2GC3sRNHcAV78Jf9wAXrqrqmkcLJ7SDyBCVS7Vrr5sCboFztfXCKUvhF8M6uJsR\nBbTf6bOByXfUfVHFgpfTe8yr7Z2ss21Gr0BEYm8CgYEAz+0f6IOZ/IyrgPLVuy9y\n/Lud0cffw8eY6n7+BGaBz8PIbY9x58dfdZ2oQUMd9AyjnqIQwOQYDMWSQgQistqG\nX4NaSMnjWxtHBK5Dmmo8psoQAFm1cUEtZQHa+wmq7eToPX19z6lOSEj7p9GgMyZu\nU8jPw2kR13I4xKuqA5FqluE=\n-----END PRIVATE KEY-----\n",
     }),
   });
 }
@@ -51,7 +51,9 @@ export default async function handler(req, res) {
     const ua = req.headers["user-agent"] || "";
 
 const isBot =
-  /facebookexternalhit|Facebot|WhatsApp|Twitterbot|TelegramBot|LinkedInBot|Discordbot|Slackbot/i.test(ua);
+  /facebookexternalhit|facebot|whatsapp|twitterbot|telegrambot|linkedinbot|discordbot|slackbot|skypeuripreview|googlebot|bingbot/i.test(
+    ua
+  );
 
 if (!isBot) {
   return res.redirect(302, redirectUrl);
